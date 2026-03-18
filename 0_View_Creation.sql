1a5dc6c0776a1f386944515f7dd63de1215f6e43
LoreB1998 <lorenzobarbato1998@gmail.com>
2026-03-18 18:14:33 +0100

    optimize cohort_analysis view for improved performance and clarity

--- 0_View_Creation.sql
+++ 0_View_Creation.sql
@@ -1,26 +1,47 @@
 DROP VIEW cohort_analysis;
 
-CREATE OR REPLACE VIEW cohort_analysis AS
+-- CREATE OR REPLACE VIEW cohort_analysis AS
 WITH customer_revenue AS (SELECT s.customerkey,
                                  s.orderdate,
                                  SUM(s.quantity * s.netprice / s.exchangerate) AS total_net_revenue,
-                                 COUNT(s.orderkey) AS num_orders,
+                                 COUNT(s.orderkey)                             AS num_orders,
                                  c.countryfull,
                                  c.age,
-                                 CONCAT(TRIM(c.givenname),' ',TRIM(c.surname)) AS cleaned_name
+                                 c.givenname,
+                                 c.surname
                           FROM sales s
                                    LEFT JOIN public.customer c on c.customerkey = s.customerkey
-                          GROUP BY s.customerkey, c.customerkey, s.orderdate)
-SELECT cr.*,
-       MIN(cr.orderdate) OVER (PARTITION BY cr.customerkey) AS first_purchase_date,
+                          GROUP BY s.customerkey, c.customerkey, s.orderdate, c.countryfull, s.orderdate, s.customerkey,
+                                   c.age, c.givenname, c.surname)
+SELECT customerkey,
+       orderdate,
+       total_net_revenue,
+       num_orders,
+       countryfull,
+       age,
+       CONCAT(TRIM(givenname), ' ', TRIM(surname))                             AS cleaned_name,
+       MIN(cr.orderdate) OVER (PARTITION BY cr.customerkey)                    AS first_purchase_date,
        EXTRACT(YEAR FROM MIN(cr.orderdate) OVER (PARTITION BY cr.customerkey)) AS cohort_year
 FROM customer_revenue cr;
 
 /*
-Total Revenue per Cohort
-*/
-SELECT cohort_year,
-       SUM(total_net_revenue)
-FROM cohort_analysis
-GROUP BY cohort_year;
+ Optimized View
+ */
+
+CREATE OR REPLACE VIEW cohort_analysis AS
+WITH customer_revenue AS (SELECT s.customerkey,
+                                 s.orderdate,
+                                 SUM(s.quantity * s.netprice / s.exchangerate)   AS total_net_revenue,
+                                 COUNT(s.orderkey)                               AS num_orders,
+                                 MAX(c.countryfull) AS countryfull,
+                                 MAX(c.age) AS age,
+                                 CONCAT(TRIM(c.givenname), ' ', TRIM(c.surname)) AS cleaned_name
+                          FROM sales s
+                                   INNER JOIN public.customer c on c.customerkey = s.customerkey
+                          GROUP BY s.customerkey, c.customerkey, s.orderdate)
+SELECT cr.*,
+       MIN(cr.orderdate) OVER (PARTITION BY cr.customerkey)                    AS first_purchase_date,
+       EXTRACT(YEAR FROM MIN(cr.orderdate) OVER (PARTITION BY cr.customerkey)) AS cohort_year
+FROM customer_revenue cr;
+
 
